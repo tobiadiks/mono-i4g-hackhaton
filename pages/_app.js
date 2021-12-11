80de65b2ab7f6e3b82e4605e043fed11e62f5ef0
tobiadiks <tobiadiks@gmail.com>
2021-12-11 19:35:06 +0100

    og:image update

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../css/index.css";
 import Head from "next/head";
 import Layout from "@components/layout";
+import yaft from "../public/yaft.png";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -11,11 +12,14 @@ function MyApp({ Component, pageProps }) {
           name="Description"
           content="Experience a fresh way to track your Income and Expense."
         />
-        <meta property="og:image" content="../public/yaft.png"/>
+        <meta property="og:image" content={yaft} />
 
-<meta property="og:title" content="Yaft"/>
+        <meta property="og:title" content="Yaft" />
 
-<meta property="og:description" content="Experience a fresh way to track your Income and Expense."/>
+        <meta
+          property="og:description"
+          content="Experience a fresh way to track your Income and Expense."
+        />
       </Head>
 
       <Component {...pageProps} />
